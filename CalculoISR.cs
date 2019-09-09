0518ff6b023075a0753d7aa2c9e76308a21b849c
gilberto.asprosgmail.com <gilberto.aspros@gmail.com>
2019-09-09 14:08:59 -0500

    Allow only numbers

--- CalculoISR.cs
+++ CalculoISR.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Calculadora_ISR
+﻿namespace Calculadora_ISR
 {
     public class CalculoISR
     {
@@ -15,74 +9,71 @@ namespace Calculadora_ISR
 
         public double Calcular(double sueldo)
         {
-            if (sueldo > 0.01) //Indicar que el sueldo es mayor a 0
+            if (sueldo <= 578.52)
             {
-                if (sueldo <= 578.52)
-                {
-                    limInferior = 0.01;
-                    cuotaFija = 0.00;
-                    excedente = 0.0192;
-                }
-                else if (sueldo <= 4910.18)
-                {
-                    limInferior = 578.53;
-                    cuotaFija = 11.11;
-                    excedente = 0.0640;
-                }
-                else if (sueldo <= 8629.20)
-                {
-                    limInferior = 4910.19;
-                    cuotaFija = 288.33;
-                    excedente = 0.1088;
-                }
-                else if (sueldo <= 10031.07)
-                {
-                    limInferior = 8629.21;
-                    cuotaFija = 692.96;
-                    excedente = 0.16;
-                }
-                else if (sueldo <= 12009.94)
-                {
-                    limInferior = 10031.08;
-                    cuotaFija = 917.26;
-                    excedente = 0.1792;
-                }
-                else if (sueldo <= 24222.31)
-                {
-                    limInferior = 12009.95;
-                    cuotaFija = 1271.87;
-                    excedente = 0.2136;
-                }
-                else if (sueldo <= 38177.69)
-                {
-                    limInferior = 24222.32;
-                    cuotaFija = 3880.44;
-                    excedente = 0.2352;
-                }
-                else if (sueldo <= 72887.50)
-                {
-                    limInferior = 38177.70;
-                    cuotaFija = 7162.74;
-                    excedente = 0.30;
-                }
-                else if (sueldo <= 97183.33)
-                {
-                    limInferior = 72887.51;
-                    cuotaFija = 17575.69;
-                    excedente = 0.32;
-                }
-                else if (sueldo <= 291550.00)
-                {
-                    limInferior = 97183.34;
-                    cuotaFija = 25350.35;
-                    excedente = 0.34;
-                }
-                else if (sueldo >= 291550.01)
-                {
-                    limInferior = 291550.01;
-                    cuotaFija = 91435.02;
-                    excedente = 0.35;
-                }
+                limInferior = 0.01;
+                cuotaFija = 0.00;
+                excedente = 0.0192;
+            }
+            else if (sueldo <= 4910.18)
+            {
+                limInferior = 578.53;
+                cuotaFija = 11.11;
+                excedente = 0.0640;
+            }
+            else if (sueldo <= 8629.20)
+            {
+                limInferior = 4910.19;
+                cuotaFija = 288.33;
+                excedente = 0.1088;
+            }
+            else if (sueldo <= 10031.07)
+            {
+                limInferior = 8629.21;
+                cuotaFija = 692.96;
+                excedente = 0.16;
+            }
+            else if (sueldo <= 12009.94)
+            {
+                limInferior = 10031.08;
+                cuotaFija = 917.26;
+                excedente = 0.1792;
+            }
+            else if (sueldo <= 24222.31)
+            {
+                limInferior = 12009.95;
+                cuotaFija = 1271.87;
+                excedente = 0.2136;
+            }
+            else if (sueldo <= 38177.69)
+            {
+                limInferior = 24222.32;
+                cuotaFija = 3880.44;
+                excedente = 0.2352;
+            }
+            else if (sueldo <= 72887.50)
+            {
+                limInferior = 38177.70;
+                cuotaFija = 7162.74;
+                excedente = 0.30;
+            }
+            else if (sueldo <= 97183.33)
+            {
+                limInferior = 72887.51;
+                cuotaFija = 17575.69;
+                excedente = 0.32;
+            }
+            else if (sueldo <= 291550.00)
+            {
+                limInferior = 97183.34;
+                cuotaFija = 25350.35;
+                excedente = 0.34;
+            }
+            else if (sueldo >= 291550.01)
+            {
+                limInferior = 291550.01;
+                cuotaFija = 91435.02;
+                excedente = 0.35;
             }
             return Resultado(sueldo, limInferior, cuotaFija, excedente, subsidio);
         }
@@ -130,7 +121,7 @@ namespace Calculadora_ISR
                 subsdo = 217.61;
             }
 
-            return (sueldo - limInf)*exce+cuoFij-subsdo;
+            return (sueldo - limInf) * exce + cuoFij - subsdo;
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Calculadora_ISR
 {
@@ -10,7 +12,9 @@ namespace Calculadora_ISR
     public partial class MainWindow : Window
     {
         private CalculoISR Calculo = new CalculoISR();
-        private static readonly Regex _regex = new Regex("[^0-9.-]+"); //Expresión regular que indica sólo números
+        private static readonly Regex _regex = new Regex("[^0-9]+"); //Expresión regular que indica sólo números
+        // Variable auxiliar para determinar cuando se debe borrar el texto que está en pantalla.
+        bool limpiarPantalla = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -18,12 +22,39 @@ namespace Calculadora_ISR
         }
 
         private void BtnISR_Click(object sender, RoutedEventArgs e)
-        {           
-            if (!_regex.IsMatch(txtSueldoMensual.Text) && !String.IsNullOrEmpty(txtSueldoMensual.Text)) // Verifica si es número
+        {       
+            
+            if (!String.IsNullOrEmpty(txtSueldoMensual.Text)) // Verifica si es es nulo o el campo está vacío
             {
-                var isr = Math.Round(Calculo.Calcular(Double.Parse(txtSueldoMensual.Text)), 2).ToString();
-                Result.Content = ("$" + isr); 
+                var isr = Double.Parse(txtSueldoMensual.Text);
+                //Operador ternario para indicar que el sueldo debe ser más de 0.01
+                Result.Content = isr >= 0.01 ? ("$" + Math.Round(Calculo.Calcular(isr), 2).ToString()) : "Ingresa un sueldo mayor a $0";                
             }   
+        }
+
+        //Sólo aceptar números y el caracter .
+        private void TxtSueldoMensual_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            if (e.Text != "." && _regex.IsMatch(e.Text))
+            {
+                e.Handled = true;
+            }
+            else if (e.Text == ".")
+            {
+                if (((TextBox)sender).Text.IndexOf(e.Text) > -1)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        //Evitar que se utilice la tecla de espacio
+        private void TxtSueldoMensual_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space && txtSueldoMensual.IsFocused == true)
+            {
+                e.Handled = true;
+            }
         }
     }
 }
